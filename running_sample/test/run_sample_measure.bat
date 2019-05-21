5ebed2e4abfc1bb47d90a6343009b6313a25eb9d
wxzhang <w.x.zhang93@gmail.com>
2019-05-21 10:36:39 +0200

    remove old running_sample folder

--- running_sample/test/run_sample_measure.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-:: run function: savecsv
-:: sequence of input: 1. DYN.INP 2. STAT1.INP 3. STAT2.INP 4. DYN.OUT
-python -m urbanwb.main_with_measure savecsv timeseries_measure.csv sample_measure_stat1.ini sample_measure_stat2.ini sample_measure_result.csv
-	
-ECHO ON
