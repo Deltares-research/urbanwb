b54a505070cd0205502d5659d02a2136af0795c4
wxzhang <w.x.zhang93@gmail.com>
2019-05-21 10:35:15 +0200

    modify running examples

--- running_sample_new/test/run_ep1.bat
+++ running_sample_new/test/run_ep1.bat
@@ -6,12 +6,12 @@ ECHO Example for Function "save-to-csv": Single run and csv all (selected result
 
 ECHO Save all results of single run to a file
 ::        module name               function    forcing.csv config1.ini        config2.ini    output.csv
-python -m urbanwb.main_with_measure save_to_csv ep_ts.csv ep_neighbourhood.ini ep_measure.ini ep1_results.csv
+python -m urbanwb.main save_to_csv ep_ts.csv ep_neighbourhood.ini ep_measure.ini ep1_results.csv
 
 
 ECHO Save selected results of single run to a file
 ::        module name               function    forcing.csv config1.ini        config2.ini    output.csv               variable to save       save_all is False meaning save selected 
-python -m urbanwb.main_with_measure save_to_csv ep_ts.csv ep_neighbourhood.ini ep_measure.ini ep1_results_selected.csv owl r_pr_swds theta_uz --save_all=False 
+python -m urbanwb.main save_to_csv ep_ts.csv ep_neighbourhood.ini ep_measure.ini ep1_results_selected.csv owl r_pr_swds theta_uz --save_all=False 
 
 ECHO Ended!
 pause
--- running_sample_new/test/run_ep2.bat
+++ running_sample_new/test/run_ep2.bat
@@ -4,13 +4,13 @@ ECHO Example for Function "batch_run_sdf": First run baseline (q = default mean 
 ECHO then batch run different q (random number [a,b,c,d,...] or [min,max,steps])
 
 ::        module name                  function      ts.csv    config1.ini          config2.ini    output.csv      random number     baseline q default
-:: python -m urbanwb.main_with_measure batch_run_sdf ep_ts.csv ep_neighbourhood.ini ep_measure.ini ep2_results.csv --q_list=[4,5]
+python -m urbanwb.main batch_run_sdf ep_ts.csv ep_neighbourhood.ini ep_measure.ini ep2_results.csv --q_list=[4,5]
 
 ::        module name                  function      ts.csv    config1.ini          config2.ini    output.csv      random number     baseline q predefined
-:: python -m urbanwb.main_with_measure batch_run_sdf ep_ts.csv ep_neighbourhood.ini ep_measure.ini ep2_results1.csv --q_list=[10,20] --baseline_q=4
+python -m urbanwb.main batch_run_sdf ep_ts.csv ep_neighbourhood.ini ep_measure.ini ep2_results1.csv --q_list=[10,20] --baseline_q=4
 
 ::        module name                  function      ts.csv    config1.ini          config2.ini    output.csv      [min,max,steps]   baseline q:predefined AP:True to enable [min,max,steps]. if False, then q_list random numbers
-:: python -m urbanwb.main_with_measure batch_run_sdf ep_ts.csv ep_neighbourhood.ini ep_measure.ini ep2_results2.csv --q_list=[4,8,3] --baseline_q=3 --arithmetic_progression=True
+python -m urbanwb.main batch_run_sdf ep_ts.csv ep_neighbourhood.ini ep_measure.ini ep2_results2.csv --q_list=[4,8,3] --baseline_q=3 --arithmetic_progression=True
 
 ECHO Ended!
 pause
